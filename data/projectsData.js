406f2b52796d6c909a3de88201df8429c2f32bf8
Robert Vásquez <vasquezrober09@gmail.com>
2023-08-22 13:13:44 -0400

    docs: :memo: add
    New two blog post and two projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -11,6 +11,18 @@ const projectsData = [
     imgSrc: '/static/images/projects/sunny-agency/sunny-agency.png',
     href: '/blog/projects/sunny-agency',
   },
+  {
+    title: 'Snap landing page',
+    description: `Frontend Mentor Challenge. Landing Page`,
+    imgSrc: '/static/images/projects/snap-landing-page/snap-landing-page.jpg',
+    href: '/blog/projects/snap-landing-page',
+  },
+  {
+    title: 'Batatabit project',
+    description: `Batatabit project is a landing page with mobile first design`,
+    imgSrc: '/static/images/projects/batatabit/batatabit.jpg',
+    href: '/blog/projects/batatabit',
+  },
 ];
 
 export default projectsData;
